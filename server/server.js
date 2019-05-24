3125bab210550dc56458d3a81117ed100b192681
AngelikaKuleta <angelcia.kuleta@gmail.com>
2019-05-24 22:32:41 +0200

    handle socket.io

--- server/server.js
+++ server/server.js
@@ -2,7 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-app.listen(3001, () => console.log('Lisening on port 3001.'));
+const server = app.listen(3001, () => console.log('Lisening on port 3001.'));
+
+const socket = require('socket.io')
+
+const io = socket(server);
+io.on('connection', (socket) => {
+   console.log('Made socket connection', socket.id);
+
+   socket.on('chat', (data) => {
+      io.sockets.emit('chat', data);
+   })
+
+   socket.on('typing', (data) => {
+      socket.broadcast.emit('typing', data);
+   });
+})
 
 mongoose.connect('mongodb+srv://admin:admin@hackathon0-h81u7.azure.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
    .then(() => console.log("Connected to MongoDB."))

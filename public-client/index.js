8cbbab9b348a4a3f0e84ac2748229db6b47d25dd
Mateusz Pichniarczyk <mat.pich@interia.pl>
2019-05-25 01:34:42 +0200

    welcome screen

---
+++ public-client/index.js
@@ -0,0 +1,68 @@
+const nickForm = document.querySelector('#nick-form');
+const nickInput = document.querySelector('#nickname');
+
+
+nickForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const username = nickInput.value;
+    document.body.innerHTML = chatWindow;
+    initializeChat(username);
+})
+
+const chatWindow = `    
+    <div id="chat-wrapper">
+        <h2 id="welcome"></h2>
+        <div id="chat-window">
+            <div id="output"></div>
+            <div id="feedback"></div>
+        </div>
+        <input id="message" type="text" placeholder="Message" />
+        <button id="send">Send</button>
+    </div>`
+
+
+const initializeChat = (username) => {
+    // Make connection
+    const socket = io.connect('http://localhost:3001');
+
+    // Query DOM
+    const message = document.getElementById('message'),
+        welcome = document.getElementById('welcome'),
+        btn = document.getElementById('send'),
+        output = document.getElementById('output'),
+        feedback = document.getElementById('feedback');
+
+    welcome.innerHTML = `Witaj ${username}, połączyłeś się z naszym konsultantem`;
+
+    // Emit events
+     const emitMessage = () => {
+         socket.emit('chat', {
+             message: message.value,
+             handle: username
+         });
+         message.value = "";
+     };
+
+    // Emit events on button click
+    btn.addEventListener('click', function () {
+        emitMessage();
+    });
+
+    // Emit events on 'Enter" press
+    message.addEventListener('keypress', function (e) {
+        socket.emit('typing', username);
+
+        //allows to send messages by pressing enter and prevents from sending empty messages
+        if(e.which === 13 && message.value !== "") emitMessage();
+    })
+
+    // Listen for events
+    socket.on('chat', function (data) {
+        feedback.innerHTML = '';
+        output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+    });
+
+    socket.on('typing', function (data) {
+        feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+    });
+}

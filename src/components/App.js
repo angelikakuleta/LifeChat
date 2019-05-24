a45b91911a823ca268baede5c643e3ff193aaa9c
angelikakuleta <kuleta.an@gmail.com>
2019-05-25 01:23:49 +0200

    add login and register components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../styles/App.css';
 
+// import Login from './Login/Login';
+import Register from './Register/Register';
+
 function App() {
   return (
-    <div className="App">
-      Hello world!
-    </div>
+    <>
+      <Register />
+    </>
   );
 }
 

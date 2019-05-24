6f3cd3ee5df351c906143baee03aad1e919de084
AngelikaKuleta <angelcia.kuleta@gmail.com>
2019-05-25 01:23:49 +0200

    add login and register components

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const register = require('./routes/register');
+const login = require('./routes/login');
 
 const app = express();
 app.listen(3001, () => console.log('Lisening on port 3001.'));
@@ -9,3 +11,5 @@ mongoose.connect('mongodb+srv://admin:admin@hackathon0-h81u7.azure.mongodb.net/t
    .catch((err) => console.log("Error", err))
 
 app.use(express.json());
+app.use('/register', register);
+app.use('/login', login);

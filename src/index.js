87f7464b539c636000c8bc2068ca320b88e58aaa
Kuba <ja.tanski@gmail.com>
2019-05-24 23:25:04 +0200

    add [mainVIew][Card]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ChatView from './components/ChatView';
-import './styles/App.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/App.css";
+import App from "./components/App";
 
-ReactDOM.render(<ChatView />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));

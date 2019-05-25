8ee009e0daf600e8337832233598f325e76d8982
AngelikaKuleta <angelcia.kuleta@gmail.com>
2019-05-25 06:17:59 +0200

    styles to inBuild component

--- src/components/InBuild/index.jsx
+++ src/components/InBuild/index.jsx
@@ -3,11 +3,13 @@ import {
    Link
 } from "react-router-dom";
 
+import './inBuild.css'
+
 export default function InBuild () {
    return (
-      <div>
-         <h1>Strona w budowie</h1>
-         <Link to="/" >
+      <div className="inbuild-view">
+         <h1 className="inbuild-info">Strona w budowie</h1>
+         <Link to="/" className="back-to-main">
             Wróć do strony głownej
          </Link>
       </div>

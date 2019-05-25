4fe5854a422765debc935314b9ec3a9b60324175
Ania Kujawa <kujawanna@gmail.com>
2019-05-25 07:31:56 +0200

    themes

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import {ShopItem} from './ShopItem';
+import Card from "antd/lib/card";
 import Avatars from './Avatars';
 import "./Shop.css";
 
 class Shop extends React.Component {
     state = {
         items : [
-            {name: "theme", price:"54" },
-            {name: "theme", price:"54" },
-            {name: "theme", price:"54" },
-            {name: "theme", price:"54" },
-            {name: "theme", price:"54" }
+            {name: "theme-dark", price:"50"},
+            {name: "theme-monster", price:"55" },
+            {name: "theme-light", price:"40"},
+            {name: "theme-snake", price:"25"},
+            {name: "theme-ghost", price:"60"}
         ],
         userMoney: "",
-        itemsBought: []
+        itemsBought: [],
+        theme: localStorage.getItem("theme"),
         }  
+    
 
     async componentDidMount() {
         const token = localStorage.getItem("x-auth-token");
@@ -38,18 +41,39 @@ class Shop extends React.Component {
         }
       }
 
-    handleButtonClick = (e, index) => {
-        if (this.state.items[index].price > this.state.user.money) {
+       handleButtonClick = async(e, index) => {
+        if (this.state.items[index].price > this.state.userMoney) {
             alert('You have not enough gold')
         } else {
-            this.setState(prevState => ({
-                user: {
-                    ...prevState.user,
-                    money: this.state.user.money - this.state.items[index].price,
-                    itemsBought:  [...this.state.user.itemsBought, this.state.items[index] ]
-                }
-                }
-            ))
+            const token = localStorage.getItem("x-auth-token");
+            const theme = this.state.items[index].name;
+            localStorage.setItem("theme", theme);
+            this.setState({ theme });
+            document.documentElement.setAttribute("data-theme", theme);
+            localStorage.setItem("theme", theme);
+            const requestHeaders = {
+              "Content-Type": "application/json; charset=UTF-8",
+              "x-auth-token": token
+            };  
+            const requestBody = {
+                gold : -this.state.items[index].price
+              };
+        
+            try {
+              let response = await fetch(`/addGold`, {
+                method: "post",
+                headers: requestHeaders,
+                body: JSON.stringify(requestBody)
+              });
+              if (response.status !== 200) throw response;
+              console.log("przyszło");
+              response = await response.json();
+              this.setState({
+                userMoney: response
+              });
+            } catch (error) {
+              console.log(error);
+            }
         }
     }
 
@@ -57,9 +81,10 @@ class Shop extends React.Component {
     render() {
         return (
             <div>
+                <Card style={{width: "40%", margin: "20px"}}>You have: {this.state.userMoney}</Card>
                 <div className='shop'>
                     {this.state.items.map((el,index) => 
-                    <ShopItem name={el.name} key={index} price={el.price} handleClick={(e) => this.handleButtonClick(e, index)} />)
+                    <ShopItem name={el.name} img={el.img} key={index} price={el.price} handleClick={(e) => this.handleButtonClick(e, index)} />)
                     }
                     <Avatars />
                 </div> 

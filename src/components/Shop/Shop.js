bdd57dd45d19cf9d762cfe8955d52340cb5824ee
Ania Kujawa <kujawanna@gmail.com>
2019-05-25 05:19:49 +0200

    up

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -12,36 +12,31 @@ class Shop extends React.Component {
             {name: "theme", price:"54" },
             {name: "theme", price:"54" }
         ],
-        user: {money: 700,
-            itemsBought: []
-        }
-       
-        } 
-    
+        userMoney: "",
+        itemsBought: []
+        }  
 
-    // async componentDidMount() {
-    //     const token = localStorage.getItem("x-auth-token");
-    //     const requestHeaders = {
-    //       "Content-Type": "application/json; charset=UTF-8",
-    //       "x-auth-token": token
-    //     };
-    
-    //     try {
-    //       let response = await fetch(`/addGold`, {
-    //         method: "get",
-    //         headers: requestHeaders,
-    //         body: {email: "test@gmail.com"}
-    //       });
-    //       if (response.status !== 200) throw response;
-    //       console.log("przyszło");
-    //       response = await response.json();
-    //       this.setState({
-    //         userMoney: response
-    //       });
-    //     } catch (error) {
-    //       console.log(error);
-    //     }
-    //   }
+    async componentDidMount() {
+        const token = localStorage.getItem("x-auth-token");
+        const requestHeaders = {
+          "Content-Type": "application/json; charset=UTF-8",
+          "x-auth-token": token
+        };  
+        try {
+          let response = await fetch(`/addGold`, {
+            method: "get",
+            headers: requestHeaders
+          });
+          if (response.status !== 200) throw response;
+          console.log("przyszło");
+          response = await response.json();
+          this.setState({
+            userMoney: response
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
 
     handleButtonClick = (e, index) => {
         if (this.state.items[index].price > this.state.user.money) {

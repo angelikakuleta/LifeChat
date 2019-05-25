2e5213adf0568ad8de6e1f145a74541eda941f38
Ania Kujawa <kujawanna@gmail.com>
2019-05-25 07:54:14 +0200

    upd

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,6 +2,12 @@ import React from 'react';
 import {ShopItem} from './ShopItem';
 import Card from "antd/lib/card";
 import Avatars from './Avatars';
+import {Button} from "antd";
+import armor from "../../assets/icons/armor.png";
+import axe from "../../assets/icons/axe.png";
+import bow from "../../assets/icons/bow.png";
+import dagger from "../../assets/icons/dagger.png";
+import sword from "../../assets/icons/sword.png";
 import "./Shop.css";
 
 class Shop extends React.Component {
@@ -11,7 +17,6 @@ class Shop extends React.Component {
             {name: "theme-monster", price:"55" },
             {name: "theme-light", price:"40"},
             {name: "theme-snake", price:"25"},
-            {name: "theme-ghost", price:"60"}
         ],
         userMoney: "",
         itemsBought: [],
@@ -81,11 +86,46 @@ class Shop extends React.Component {
     render() {
         return (
             <div>
-                <Card style={{width: "40%", margin: "20px"}}>You have: {this.state.userMoney}</Card>
+                <Card style={{width: "40%", margin: "20px", padding: "10px"}}>Twój zasób: {this.state.userMoney}</Card>
                 <div className='shop'>
                     {this.state.items.map((el,index) => 
                     <ShopItem name={el.name} img={el.img} key={index} price={el.price} handleClick={(e) => this.handleButtonClick(e, index)} />)
                     }
+                    <Card >
+                    <img src={armor} alt="armor" style={{"width":"50%", "height":"50%"}} />
+                     <p className="price">Cena: 100</p>
+                    <div className="btn-wrapper">
+                    <Button onClick={this.handleButtonClick}>Buy</Button>
+                        </div>
+                     </Card>
+                     <Card >
+                    <img src={axe} alt="axe" style={{"width":"50%", "height":"50%"}} />
+                     <p className="price">Cena: 80</p>
+                    <div className="btn-wrapper">
+                    <Button onClick={this.handleButtonClick}>Buy</Button>
+                        </div>
+                     </Card>
+                     <Card >
+                    <img src={bow} alt="bow" style={{"width":"50%", "height":"50%"}} />
+                     <p className="price">Cena: 120</p>
+                    <div className="btn-wrapper">
+                    <Button onClick={this.handleButtonClick}>Buy</Button>
+                        </div>
+                     </Card>
+                     <Card >
+                    <img src={dagger} alt="dagger" style={{"width":"50%", "height":"50%"}} />
+                     <p className="price">Cena: 60</p>
+                    <div className="btn-wrapper">
+                    <Button onClick={this.handleButtonClick}>Buy</Button>
+                        </div>
+                     </Card>
+                     <Card >
+                    <img src={sword} alt="sword" style={{"width":"50%", "height":"50%"}} />
+                     <p className="price">Cena: 150</p>
+                    <div className="btn-wrapper">
+                    <Button onClick={this.handleButtonClick}>Buy</Button>
+                        </div>
+                     </Card>
                     <Avatars />
                 </div> 
             </div>

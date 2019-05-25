90c11ac3fbe045680e381c35d273a556b8fd4418
Kuba <ja.tanski@gmail.com>
2019-05-25 03:41:19 +0200

    change png

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
 import Card from "antd/lib/card";
-import { Avatar } from 'antd';
+import { Avatar } from "antd";
 import "./Card.css";
 import "../../styles/App.css";
+import avatar1 from "../../assets/avatars/pipo-enemy1.png";
 
 export default class CardView extends Component {
   state = {};
   render() {
     return (
       <Card className="card">
-      <div className="wrapper">
-        <Avatar className="avatar" size={100} icon="user" />
+        <div className="wrapper">
+          <Avatar className="avatar" size={100} src={avatar1} />
           <div className="info">
             <div className="data">
               <p className="name">{this.props.name}</p>
               <p>{this.props.date}</p>
             </div>
             {/* <p>Priorytet: {this.props.priority}</p> */}
-            <p>{this.props.message}</p>          
-          </div>          
-      </div>
+            <p>{this.props.message}</p>
+          </div>
+        </div>
       </Card>
     );
   }

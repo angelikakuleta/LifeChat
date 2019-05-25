b3cdb68b026956169ba75e39bdb1ed5effa9a530
Ania Kujawa <kujawanna@gmail.com>
2019-05-25 02:31:45 +0200

    shop starter

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "../styles/App.css";
 import MainView from "./mainView/mainView";
+import Shop from "./Shop/Shop";
 import Menu from "./Menu";
 
 function App() {
   return (
     <div>
-    <Menu />;
-    <MainView />;
+    <Menu />
+    {/* <MainView />; */}
+    <Shop />
     </div>
   )
 }

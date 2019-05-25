3eb5b1c2c7f62f3a54dc5d1a0346b745b4da9924
Ania Kujawa <kujawanna@gmail.com>
2019-05-25 08:32:58 +0200

    shop with weapons

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {ShopItem} from './ShopItem';
 import Card from "antd/lib/card";
 import Avatars from './Avatars';
-import {Button} from "antd";
 import armor from "../../assets/icons/armor.png";
 import axe from "../../assets/icons/axe.png";
 import bow from "../../assets/icons/bow.png";
 import dagger from "../../assets/icons/dagger.png";
 import sword from "../../assets/icons/sword.png";
+import heart from "../../assets/icons/heart.png";
+import star from "../../assets/icons/star.png";
+import {ShopWeapon} from './ShopWeapon';
 import "./Shop.css";
 
 class Shop extends React.Component {
@@ -17,6 +19,15 @@ class Shop extends React.Component {
             {name: "theme-monster", price:"55" },
             {name: "theme-light", price:"40"},
             {name: "theme-snake", price:"25"},
+        ],
+        weapons : [
+            {price:100},
+            {price:80},
+            {price:120},
+            {price:60},
+            {price:150},
+            {price: 200},
+            {price: 75}
         ],
         userMoney: "",
         itemsBought: [],
@@ -81,6 +92,41 @@ class Shop extends React.Component {
             }
         }
     }
+    handleItemClick = async(e, index) => {
+        if (this.state.weapons[index].price > this.state.userMoney) {
+            alert('You have not enough gold')
+        } else {
+            const token = localStorage.getItem("x-auth-token");
+            const theme = this.state.items[index].name;
+            localStorage.setItem("theme", theme);
+            this.setState({ theme });
+            document.documentElement.setAttribute("data-theme", theme);
+            localStorage.setItem("theme", theme);
+            const requestHeaders = {
+              "Content-Type": "application/json; charset=UTF-8",
+              "x-auth-token": token
+            };  
+            const requestBody = {
+                gold : -this.state.items[index].price
+              };
+        
+            try {
+              let response = await fetch(`/addGold`, {
+                method: "post",
+                headers: requestHeaders,
+                body: JSON.stringify(requestBody)
+              });
+              if (response.status !== 200) throw response;
+              console.log("przyszło");
+              response = await response.json();
+              this.setState({
+                userMoney: response
+              });
+            } catch (error) {
+              console.log(error);
+            }
+        }
+    }
 
 
     render() {
@@ -89,43 +135,15 @@ class Shop extends React.Component {
                 <Card style={{width: "40%", margin: "20px", padding: "10px"}}>Twój zasób: {this.state.userMoney}</Card>
                 <div className='shop'>
                     {this.state.items.map((el,index) => 
-                    <ShopItem name={el.name} img={el.img} key={index} price={el.price} handleClick={(e) => this.handleButtonClick(e, index)} />)
+                    <ShopItem name={el.name} key={index} price={el.price} handleClick={(e) => this.handleButtonClick(e, index)} />)
                     }
-                    <Card >
-                    <img src={armor} alt="armor" style={{"width":"50%", "height":"50%"}} />
-                     <p className="price">Cena: 100</p>
-                    <div className="btn-wrapper">
-                    <Button onClick={this.handleButtonClick}>Buy</Button>
-                        </div>
-                     </Card>
-                     <Card >
-                    <img src={axe} alt="axe" style={{"width":"50%", "height":"50%"}} />
-                     <p className="price">Cena: 80</p>
-                    <div className="btn-wrapper">
-                    <Button onClick={this.handleButtonClick}>Buy</Button>
-                        </div>
-                     </Card>
-                     <Card >
-                    <img src={bow} alt="bow" style={{"width":"50%", "height":"50%"}} />
-                     <p className="price">Cena: 120</p>
-                    <div className="btn-wrapper">
-                    <Button onClick={this.handleButtonClick}>Buy</Button>
-                        </div>
-                     </Card>
-                     <Card >
-                    <img src={dagger} alt="dagger" style={{"width":"50%", "height":"50%"}} />
-                     <p className="price">Cena: 60</p>
-                    <div className="btn-wrapper">
-                    <Button onClick={this.handleButtonClick}>Buy</Button>
-                        </div>
-                     </Card>
-                     <Card >
-                    <img src={sword} alt="sword" style={{"width":"50%", "height":"50%"}} />
-                     <p className="price">Cena: 150</p>
-                    <div className="btn-wrapper">
-                    <Button onClick={this.handleButtonClick}>Buy</Button>
-                        </div>
-                     </Card>
+                    <ShopWeapon src={armor} price={this.state.weapons[0].price} handleClick={(e) => this.handleItemClick(e, 0)} ></ShopWeapon>
+                    <ShopWeapon src={axe} price={this.state.weapons[1].price} handleClick={(e) => this.handleItemClick(e, 0)} ></ShopWeapon>
+                    <ShopWeapon src={bow} price={this.state.weapons[2].price} handleClick={(e) => this.handleItemClick(e, 0)} ></ShopWeapon>
+                    <ShopWeapon src={dagger} price={this.state.weapons[3].price} handleClick={(e) => this.handleItemClick(e, 0)} ></ShopWeapon>
+                    <ShopWeapon src={sword} price={this.state.weapons[4].price} handleClick={(e) => this.handleItemClick(e, 0)} ></ShopWeapon>
+                    <ShopWeapon src={heart} price={this.state.weapons[5].price} handleClick={(e) => this.handleItemClick(e, 0)} ></ShopWeapon>
+                    <ShopWeapon src={star} price={this.state.weapons[6].price} handleClick={(e) => this.handleItemClick(e, 0)} ></ShopWeapon>                  
                     <Avatars />
                 </div> 
             </div>

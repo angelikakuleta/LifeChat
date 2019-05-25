fcd41bcf0574d39a7d594c4b3112d27527f12ec8
CaptainObjective <marcinwos18@gmail.com>
2019-05-25 02:05:36 +0200

    addKeywords

--- server/routes/addKeyWord.js
+++ server/routes/addKeyWord.js
@@ -5,13 +5,15 @@ const { User } = require('../models/userModel');
 
 router.post('/', async (req, res) => {
     let user;
-    console.log(User);
-    // user = await User.findOne({ email: req.body.email.toLowerCase() });
+    user = await User.findOne({ email: req.body.email.toLowerCase() });
     if (!user) return res.status(400).send('Invalid email or password.')
-
+    console.log(req);
     try {
         let response = await User.findOneAndUpdate({ email: req.body.email.toLowerCase() }, { keywords: req.body.keywords })
-            (response) ? res.send(200) : res.send(404);
+        if (response) {
+            console.log(response);
+            res.send(200);
+        }
     } catch (err) {
 
     }
